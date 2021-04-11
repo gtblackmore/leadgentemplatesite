e246a6b7e2670fe409b5af143b5e9e9106016a9d
gtblackmore <gtblackmore@gmail.com>
2021-04-11 15:29:20 -0400

    update Hero component

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Hero extends React.Component {
   render() {
     return (
-      <div class="ui container">
+      <>
         <img class="ui fluid image" src="../hero-placeholder.png" />
-      </div>
+      </>
     )}
 }
 
